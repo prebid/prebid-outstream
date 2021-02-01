a78667c62f779423a8dc086db276a6aa2a1e4e37
Peter <peter@orangeclickmedia.com>
2021-02-01 16:54:10 +0200

    exposing window.outstreamPlayer as an object with .player being the actual function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import OutstreamPlayer from './OutstreamPlayer';
 import logger from './Logger';
 
 // eslint-disable-next-line no-unused-vars
-window.outstreamPlayer = window.outstreamPlayer || ( (bid, elementId, config) =>  {
+var player = window.outstreamPlayer || ( (bid, elementId, config) =>  {
     logger.log("Inside window.outstreamPlayer.");
     return new OutstreamPlayer(bid, elementId, config);
 });
+
+export { player };
