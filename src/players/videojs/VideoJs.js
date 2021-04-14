20c8c48de9aecd6e45e6400718b4cf89a8211d14
Brian Thai <brian.thai@rdbrck.com>
2021-04-14 13:04:39 -0700

    Added videojs

--- src/players/videojs/VideoJs.js
+++ src/players/videojs/VideoJs.js
@@ -1,33 +1,83 @@
+import videojs from 'video.js';
+
+// Manually load in window.videojs
+window.videojs = videojs;
+// Use require to force vast plugin to load in after video.js
+require('@prebid/videojs-vast/bin/videojs_5.vast.vpaid');
+import 'video.js/dist/video-js.css';
+
 import GenericPlayer from '../GenericPlayer';
 import logger from '../../Logger';
-
+import VideoJsConfig from './VideoJsConfig';
 
 export default class VideoJs extends GenericPlayer{
     constructor(){
         super();
         logger.debug("Inside videojs player constructor.");
+        logger.log("Player object: ", videojs);
+        this.playerConfig = null;
+        this.player = null;
+        this.bid = null;
+        this.elementId = null;
+        this.element = null;
+        this.isVideoPlaying = false;
     }
 
     generatePlayerConfig(bid, elementId, genericConfiguration){
-        logger.debug("Inside VideoJs.generatePlayerConfig method.");
+        logger.debug("Inside generatePlayerConfig");
         this.bid = bid;
+
+        // Check if valid elementId
+        if(document.getElementById(elementId) === null){
+            logger.error("No element present with element ID: " + elementId);
+            throw new Error('Please provide a valid element ID.');
+        }
+
         this.elementId = elementId;
+        this.element = document.getElementById(this.elementId);
+
         this.genericConfiguration = genericConfiguration;
+        logger.log("configuration: ", JSON.stringify( this.genericConfiguration));
+
+        this.playerConfig = new VideoJsConfig(this.bid, this.elementId, this.genericConfiguration);
+        logger.log("this.playerConfig: " + JSON.stringify(this.playerConfig));
     }
 
     setupPlayer(videoPlayerId){
-        logger.debug("Inside VideoJs.generatePlayerConfig method with player ID: " + videoPlayerId);
+        logger.debug("Inside setupPlayer with player ID: ", videoPlayerId);
+
+        if(document.getElementById(videoPlayerId) ===  null){
+            logger.error("No element is present with videoPlayerId: ", videoPlayerId);
+            throw new Error('Please provide a valid video player ID.');
+        }
+
+        const video = document.getElementById(videoPlayerId);
+
+        video.className += ' video-js';
+        logger.log("adding videojs to element", videoPlayerId, this.element.className);
+
+        this.player = videojs(videoPlayerId, this.playerConfig);
+        this.player.src("//d2zihajmogu5jn.cloudfront.net/tiny.mp4");
+        this.player.on('error', e => console.log('vjs error:', e));
+        this.player.on('vast.adError', e => console.log('vjs ad error:', e));
+        this.player.on(["adFinished", "vast.contentStart"], () => {
+            this.player.pause(); // don't play the video ever
+            this.player.dispose();
+        });
     }
 
     play(){
-        logger.debug("Inside VideoJs.play");
+        logger.debug("player play");
+        this.player.play();
     }
 
     pause(){
-        logger.debug("Inside VideoJs.pause");
+        logger.debug("player pause");
+        this.player.pause();
     }
 
     getIsVideoPlaying(){
-        logger.debug("Inside VideoJs.getIsVideoPlaying");
+        logger.debug("player getIsVideoPlaying. Returning the value: ", this.isVideoPlaying);
+        return this.isVideoPlaying;
     }
 }
---
+++ src/players/videojs/VideoJsConfig.js
@@ -0,0 +1,47 @@
+import logger from '../../Logger';
+import merge from 'lodash.merge'
+
+export default class VideoJsConfig{
+    constructor(bid, elementId, genericConfiguration){
+        videojs.log.level('debug');
+        this.controls = true;
+        this.autoplay = true;
+        this.preload = 'none';
+        this.height = genericConfiguration.height;
+        this.width = genericConfiguration.width;
+        this.muted = genericConfiguration.mute;
+        this.plugins = {
+            vastClient: {
+                adsEnabled: true,
+                playAdAlways: true,
+                verbosity: 4,
+                initialAudio: 'off',
+            }
+        };
+
+        // Allow player config overrides
+        merge(this, genericConfiguration.playerOverrides);
+
+        let ad;
+        if( !(bid.ad === undefined || bid.ad === null) ){
+            logger.log("Bid object contains bid.ad with value: " + bid.ad);
+            ad = bid.ad;
+        }else if( !(bid.vastXml === undefined || bid.vastXml === null) ){
+            logger.log("Bid object contains bid.vastXml with value: " + bid.vastXml);
+            ad = bid.vastXml;
+        }
+
+        if (ad) {
+            if (ad.search(/<VAST/ig) !== -1) {
+                // No direct support for VAST tag, so creating a Data URI
+                logger.log("VAST type creative: " + ad);
+                this.plugins.vastClient.adTagXML = callback => callback(null, ad);
+            } else if (ad.startsWith('http')) {
+                logger.log("Ad tag type creativrr: " + ad);
+                this.plugins.vastClient.adTagUrl = ad;
+            }
+        } else {
+            logger.error("Bid object doesn't contain bid.ad or bid.vastXml object, so no VAST tag or XML is available.");
+        }
+    }
+}
